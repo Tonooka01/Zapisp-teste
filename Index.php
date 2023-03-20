4dd8185d9f2fb4877ff1c9888e5088161e14711f
Tonooka01 <123206032+Tonooka01@users.noreply.github.com>
2023-03-20 12:51:43 -0300

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,3 +1,22 @@
 <?php
-Echo "Teste"
-?>
+$mensagem = 'Meu primeiro teste com a API do zapisp!';
+$telefone = '5512988487645';
+$apiToken = '150d53f78cc8057424da0c27cda93f2f';
+$instancia_id = '52';
+$api = "https://zapisp.com.br/api/whatsapp/v2/52";
+
+$ch = curl_init();curl_setopt($ch, CURLOPT_URL, $api);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, [
+    'apiToken' => $apiToken,
+    'telefone' => $telefone,
+    'mensagem' => $mensagem,
+    'instancia' => $instancia_id
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+print_r($response);
+
+echo "Mensagem enviado com sucesso";
